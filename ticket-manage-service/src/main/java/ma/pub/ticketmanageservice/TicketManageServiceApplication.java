33c1ddd5e9e989db126c3c3159a52d357d689ed8
HAMID ISMAINI <ismaini.hamid@gmail.com>
2025-02-23 18:36:00 +0000

    complete

--- ticket-manage-service/src/main/java/ma/pub/ticketmanageservice/TicketManageServiceApplication.java
+++ ticket-manage-service/src/main/java/ma/pub/ticketmanageservice/TicketManageServiceApplication.java
@@ -1,5 +1,8 @@
 package ma.pub.ticketmanageservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -8,6 +11,17 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Manage tickets APIs",
+                version = "1.0.0",
+                description = "APIs for manage tickets."
+        ),
+        servers = {
+                @Server(url = "http://localhost:8080/api", description = "Local development server"),
+                @Server(url = "http://localhost:8080/api", description = "Production server")
+        }
+)
 public class TicketManageServiceApplication {
 
     public static void main(String[] args) {
